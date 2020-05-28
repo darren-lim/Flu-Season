c1f8ff7dc50b5b4d82096f07ff1687f384107e31
Darren <dartrenbbx@gmail.com>
2020-05-28 15:05:38 -0700

    index

--- Assets/Darren's Assets/D_Scripts/D_EnemyAbstract.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-abstract public class D_EnemyAbstract : MonoBehaviour
-{
-    abstract public void TakeDamage();
-}
